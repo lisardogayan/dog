e579c4fb015af5736dd4b09efb259f161993e696
lisardogayan <lisardogayan@gmail.com>
2019-10-21 22:48:51 +0000

    Instances are added and checked

--- dog.py
+++ dog.py
@@ -12,7 +12,16 @@ mikey = Dog("Rufus", 6)
 print("{} is {} and {} is {}.".format(
     bambi.name, bambi.age, mikey.name, mikey.age))
 
-if bambi.species == "mammcanicheal":
+# comparison is changed from "mammcanicheal" to "caniche"
+if bambi.species == "caniche":
     print("{0} is a {1}!".format(bambi.name, bambi.species))
 
+# the three instances are created
+bluedog = Dog("Chase", 4)
+reddog = Dog("Marshall", 7)
+yellowdog = Dog("Rubble", 8)
 
+# the instances are checked
+dogs = [bluedog, reddog, yellowdog]
+for dog in dogs:
+    print("{0} is a {1} of {2} years old!".format(dog.name, dog.species, dog.age))
